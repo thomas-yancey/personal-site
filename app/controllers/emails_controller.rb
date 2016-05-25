ea95b38c9c896ae911c04ff22a1a37cac332ac2a
thomas-yancey <tomyncy618@gmail.com>
2016-05-25 16:02:38 -0400

    error messages for emails and email validations

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,14 +4,18 @@ class EmailsController < ApplicationController
   end
 
   def create
+    flash.clear
     email = Email.new(message_params)
     if email.save
       ContactMailer.contact_email(email).deliver_later
       flash[:notice] = "Thanks for reaching out! I will get back to you shortly"
+      @email = Email.new
     else
+      flash[:danger] = "Your message was unable to process, please check the following"
       @errors = email.errors.full_messages
+      @email = email
     end
-    redirect_to root_path
+    render "/welcome/index"
   end
 
   private
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,6 @@
 class Email < ActiveRecord::Base
+
+  validates_format_of :address,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :message, presence: true
+
 end
